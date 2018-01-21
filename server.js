3b739e32a11166a9a267c3941c543062aac8bd42
Boris Borisov <borisborisov@Boriss-MacBook-Pro-2.local>
2018-01-21 11:36:07 +0200

    check if the request forwards to http

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
-    if (req.headers['x-forwarded-proto']) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
         next();
     } else {
         res.redirect('http://' + req.hostname + req.url);
