2c45956d33b4e3c2aa0cdc4062bc37f7e5803029
Boris Borisov <borisborisov@rw-fg-3.westernacher.com>
2018-01-21 14:50:43 +0200

    add foundation css lib

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,10 @@ const Weather = require('./components/Weather');
 const About = require('./components/About');
 const Examples = require('./components/Examples');
 
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path='/' component={Main}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './app/app.jsx',
+    entry: [
+        'script!jquery/dist/jquery.min.js',
+        'script!foundation-sites/dist/foundation.min.js',
+        './app/app.jsx'
+    ],
+    externals: {
+        jquery: 'jQuery'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            'jQuery': 'jquery'
+        })
+    ],
     output: {
         path: __dirname,
         filename: './app/bundle.js'
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['', '.js', '.jsx']
     },
     devtool: 'source-map',
     module: {
